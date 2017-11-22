2f1f3e2fff8394f21ecf25574200efa0ad8b970b
LiemLe <lthanhliem38@gmail.com>
2017-11-22 14:53:31 +0700

    Update ArtifactId

--- src/main/java/ups/mongo/MongoConnectionBootApplication.java
+++ src/main/java/ups/mongo/MongoConnectionBootApplication.java
@@ -1,5 +1,6 @@
 package ups.mongo;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
